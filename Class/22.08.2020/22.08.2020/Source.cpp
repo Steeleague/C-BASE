2c5db643411f9420225212de56dede29c242b941
MKA <mka@rivne.itstep.org>
2020-08-22 17:57:33 +0300

    classwork

--- Class/22.08.2020/22.08.2020/Source.cpp
+++ Class/22.08.2020/22.08.2020/Source.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
-
+#include <ctime>
 using namespace std;
 
-int main{
-cout << "he" << endl;
+int main(){
+	srand(unsigned(time(NULL)));
+	const int a = 30;
+
+	int arr[a];
+	for (int i = 0; i < a; i++) {
+		arr[i] = rand() % 200 - 100;
+		
+			cout << "ABC [" << i << "] = " << arr[i] << endl;
+		
+	}
+	
 }
 
 
